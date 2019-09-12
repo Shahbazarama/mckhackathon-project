0a459c8af0f630c4bb275bb624406ceb73385892
Raffi Shahbazian <raffi.p.shahbazian@gmail.com>
2019-09-12 11:45:01 -0700

    card

--- src/Components/Conversation.jsx
+++ src/Components/Conversation.jsx
@@ -4,7 +4,27 @@ export default function Conversation({ sentimentalValue }) {
 
   return (
     <>
-      {sentimentalValue}
+    {( () => {
+      switch(true) {
+        case (sentimentalValue < 3):
+          return <h1>Good luck</h1>;
+          break;
+        case (sentimentalValue < 5):
+          return <h1>Be calm</h1>;
+          break;
+        case (sentimentalValue == 5):
+          return <h1>Stay positive</h1>;
+          break;
+        case (sentimentalValue < 8):
+          return <h1>Keep it up!</h1>;
+          break;
+        case (sentimentalValue > 7):
+          return <h1>Great job!</h1>;
+          break;
+        default:
+          return null;
+      }
+    })()}
     </>
   )
 }
--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Row, Col, Button } from 'reactstrap'
+import { Row, Col, Button, Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle } from 'reactstrap'
 import { FaFrownOpen, FaFrown, FaSmile, FaSmileBeam, FaLaugh } from 'react-icons/fa';
 import Conversation from './Conversation.jsx'
 
@@ -11,29 +11,26 @@ export default function Game(){
     <div className="container">
       <Row>
         <Col className="text-center">
-          <h5>
-            {sentimentalValue}
-          </h5>
           <h1>
             {( () => {
               switch(true) {
                 case (sentimentalValue < 3):
-                  return <FaFrownOpen  style={{'color': 'red'}} />;
-                  break;
+                return <FaFrownOpen  style={{'color': 'red'}} />;
+                break;
                 case (sentimentalValue < 5):
-                  return <FaFrown style={{'color': 'red'}} />;
-                  break;
+                return <FaFrown style={{'color': 'red'}} />;
+                break;
                 case (sentimentalValue == 5):
-                  return <FaSmile style={{'color': 'gray'}} />;
-                  break;
+                return <FaSmile style={{'color': 'gray'}} />;
+                break;
                 case (sentimentalValue < 8):
-                  return <FaSmileBeam style={{'color': 'green'}} />;
-                  break;
+                return <FaSmileBeam style={{'color': 'green'}} />;
+                break;
                 case (sentimentalValue > 7):
-                  return <FaLaugh style={{'color': 'green'}} />;
-                  break;
+                return <FaLaugh style={{'color': 'green'}} />;
+                break;
                 default:
-                  return null;
+                return null;
               }
             })()}
           </h1>
@@ -55,7 +52,20 @@ export default function Game(){
 
       </Row>
       <Row>
-        <Conversation sentimentalValue={sentimentalValue}/>
+        <Col md={6}>
+          <Conversation sentimentalValue={sentimentalValue}/>
+        </Col>
+        <Col md={6}>
+          <Card>
+            <CardImg top width="100%" src="placeholderPerson.jpg" alt="person" />
+            <CardBody>
+              <CardTitle>Corban Caller</CardTitle>
+              <CardSubtitle>(480) 555-5555</CardSubtitle>
+              <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+              <Button>Previous Call History</Button>
+            </CardBody>
+          </Card>
+        </Col>
       </Row>
     </div>
   )
